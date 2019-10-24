4e154f0c229cd14951003e2be4eac93106220e76
Ancelin GLORIES <ancelin.glories@gmail.com>
2019-10-24 16:50:06 +0200

    Add project

---
+++ src/main/java/fr/ut2j/mlice/ci/tpmaven/App.java
@@ -0,0 +1,13 @@
+package fr.ut2j.mlice.ci.tpmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
